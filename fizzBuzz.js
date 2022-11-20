acadad73548a8256d85ec65e17e0e443e65f5659
Daniyar <yeskaliyevdan@hotmail.com>
2022-11-20 12:25:35 -0800

    fizzBuzz with while

---
+++ fizzBuzz.js
@@ -0,0 +1,19 @@
+var fizzBuzz = function(n) {
+    let answer = [];
+
+    while(n) {
+        if(!(n % 15)) {
+            answer.unshift('FizzBuzz');
+        } else if(!(n % 3)) {
+            answer.unshift('Fizz');
+        } else if(!(n % 5)) {
+            answer.unshift('Buzz');
+        } else {
+            answer.unshift('' + n);
+        }
+
+        n--;
+    }
+
+    return answer;
+}
