1bf89c4f48e4d18a53ba58dd851e7cfe30a2c89e
Daniyar <85423896+daniyar-yes@users.noreply.github.com>
2024-09-14 12:28:24 -0700

    add flowerbed adjacent task (#4)
    * add flowerbed adjacent task
    
    * fix empty lines
    
    ---------
    
    Co-authored-by: Daniyar Yeskaliyev <daniyar@Daniyars-MacBook-Pro.local>

---
+++ algos/flowerbed.ts
@@ -0,0 +1,19 @@
+function canPlaceFlowers(flowerbed: number[], n: number): boolean {
+    let unplanted = n;
+
+    for (let i = 0; i < flowerbed.length; i++) {
+        if (unplanted === 0) break;
+
+        if (flowerbed[i] == 1) {
+            i++;
+            continue;
+        }
+
+        if (!flowerbed[i-1] && !flowerbed[i+1]) {
+         i++;
+         unplanted--;
+        }
+    }
+
+    return !unplanted;
+};
