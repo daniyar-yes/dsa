18b14a6b1851f3e3c93cf5f6875306edaa2106a4
daniyar-yes <yeskaliyevdan@hotmail.com>
2024-10-13 21:30:05 -0700

    max ones sliding window 2 pointers

---
+++ maxConsecutiveOnes.js
@@ -0,0 +1,20 @@
+var longestOnes = function(nums, k) {
+    let result = 0;
+    let left = 0;
+    let right = 0;
+
+    while (right < nums.length) {
+       if (nums[right] === 0) {
+           k -=1;
+       }
+
+       if (k < 0) {
+           k = nums[left] === 0 ? k + 1 : k;
+           left +=1;
+       }
+
+       right += 1;
+    }
+
+    return right - left;
+};
