829673609242513399a316b24a0cedeb35234771
daniyar-yes <yeskaliyevdan@hotmail.com>
2024-10-21 21:39:29 -0700

    removed redundand variables

--- uniqueNumberOfOccurencesMapSets.js
+++ uniqueNumberOfOccurencesMapSets.js
@@ -2,16 +2,14 @@ var uniqueOccurrences = function(arr) {
     if (arr.length === 1) return true;
 
     let occurences = new Map();
-    let isUnique = false;
 
     for (let i=0; i<arr.length; i++) {
-        let current = arr[i];
-        if (!occurences.has(current)) {
-            occurences.set(current, 0)
+        if (!occurences.has(arr[i])) {
+            occurences.set(arr[i], 0)
         }
-        if (occurences.has(current)) {
-            let count = occurences.get(current);
-            occurences.set(current, count+1)
+        if (occurences.has(arr[i])) {
+            let count = occurences.get(arr[i]);
+            occurences.set(arr[i], count+1)
         }
     }
 
