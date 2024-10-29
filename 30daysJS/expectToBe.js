da23c4d8209aadb55fb540d1d1ce0cdcddb0bb29
daniyar-yes <yeskaliyevdan@hotmail.com>
2024-10-29 14:48:55 -0700

    expect to be val1==val2

---
+++ 30daysJS/expectToBe.js
@@ -0,0 +1,23 @@
+var expect = function(val) {
+    function toBe(val2) {
+        if (val === val2) {
+            return true;
+        } else {
+            throw new Error("Not Equal");
+        }
+    }
+
+    function notToBe(val2) {
+        if (val !== val2) {
+            return true;
+        } else {
+            throw new Error("Equal")
+        }
+    }
+    return (
+        {
+            toBe: toBe,
+            notToBe: notToBe
+        }
+    )
+};
